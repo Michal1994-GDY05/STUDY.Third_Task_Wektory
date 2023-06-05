a59aa260b856c444a7c44bf054af58e42ba8f7f3
Michal Jakusz <jakusz.it@gmail.com>
2023-06-05 11:41:34 +0200

    Dodaj pliki projektów.

---
+++ ObliczeniaWektorów.cs
@@ -0,0 +1,98 @@
+﻿namespace Third_Task_Wektory
+{
+    internal class ObliczeniaWektorów
+    {
+
+        public static double IloczynSkalarny(Wektor v1, Wektor v2)
+        {
+            if (v1.Wymiar != v2.Wymiar)
+                throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+
+            double suma = 0;
+            for (byte i = 0; i < v1.Wymiar; i++)
+            {
+                suma += v1[i] * v2[i];
+            }
+            return suma;
+        }
+
+        public static Wektor Suma(params Wektor[] wektory)
+        {
+            if (wektory.Length == 0)
+                throw new ArgumentException("Nie podano żadnego wektora.");
+
+            byte wymiar = wektory[0].Wymiar;
+            for (int i = 1; i < wektory.Length; i++)
+            {
+                if (wektory[i].Wymiar != wymiar)
+                    throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+            }
+
+            Wektor wynik = new Wektor(wymiar);
+            for (byte i = 0; i < wymiar; i++)
+            {
+                double suma = 0;
+                for (int j = 0; j < wektory.Length; j++)
+                {
+                    suma += wektory[j][i];
+                }
+                wynik[i] = suma;
+            }
+            return wynik;
+        }
+
+        public static Wektor Dodaj(Wektor v1, Wektor v2)
+        {
+            if (v1.Wymiar != v2.Wymiar)
+                throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+
+            Wektor wynik = new Wektor(v1.Wymiar);
+            for (byte i = 0; i < v1.Wymiar; i++)
+            {
+                wynik[i] = v1[i] + v2[i];
+            }
+            return wynik;
+        }
+
+        public static Wektor Odejmij(Wektor v1, Wektor v2)
+        {
+            if (v1.Wymiar != v2.Wymiar)
+                throw new ArgumentException("Wektory muszą mieć ten sam wymiar.");
+
+            Wektor wynik = new Wektor(v1.Wymiar);
+            for (byte i = 0; i < v1.Wymiar; i++)
+            {
+                wynik[i] = v1[i] - v2[i];
+            }
+            return wynik;
+        }
+
+        public static Wektor Pomnóż(Wektor v, double skalar)
+        {
+            Wektor wynik = new Wektor(v.Wymiar);
+            for (byte i = 0; i < v.Wymiar; i++)
+            {
+                wynik[i] = v[i] * skalar;
+            }
+            return wynik;
+        }
+
+        public static Wektor Pomnóż(double skalar, Wektor v)
+        {
+            return Pomnóż(v, skalar);
+        }
+
+        public static Wektor Podziel(Wektor v, double skalar)
+        {
+            if (skalar == 0)
+                throw new ArgumentException("Dzielenie przez zero.");
+
+            Wektor wynik = new Wektor(v.Wymiar);
+            for (byte i = 0; i < v.Wymiar; i++)
+            {
+                wynik[i] = v[i] / skalar;
+            }
+            return wynik;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Third_Task_Wektory;
+
+public class Wektor
+{
+    private double[] współrzędne;
+
+    public Wektor(byte wymiar)
+    {
+        współrzędne = new double[wymiar];
+    }
+
+    public Wektor(params double[] współrzędne)
+    {
+        this.współrzędne = współrzędne;
+    }
+
+    public double this[byte indeks]
+    {
+        get { return współrzędne[indeks]; }
+        set { współrzędne[indeks] = value; }
+    }
+
+    public double Długość
+    {
+        get { return Math.Sqrt(ObliczeniaWektorów.IloczynSkalarny(this, this)); }
+    }
+
+    public byte Wymiar
+    {
+        get { return (byte)współrzędne.Length; }
+    }
+
+    public static Wektor Suma(params Wektor[] wektory)
+    {
+        return ObliczeniaWektorów.Suma(wektory);
+    }
+
+    public static Wektor operator +(Wektor v1, Wektor v2)
+    {
+        return ObliczeniaWektorów.Dodaj(v1, v2);
+    }
+
+    public static Wektor operator -(Wektor v1, Wektor v2)
+    {
+        return ObliczeniaWektorów.Odejmij(v1, v2);
+    }
+
+    public static Wektor operator *(Wektor v, double skalar)
+    {
+        return ObliczeniaWektorów.Pomnóż(v, skalar);
+    }
+
+    public static Wektor operator *(double skalar, Wektor v)
+    {
+        return ObliczeniaWektorów.Pomnóż(skalar, v);
+    }
+
+    public static Wektor operator /(Wektor v, double skalar)
+    {
+        return ObliczeniaWektorów.Podziel(v, skalar);
+    }
+
+
+
+    public override string ToString()
+    {
+        string separator = " | ";
+        string result = string.Join(separator, współrzędne);
+        return $"({result})";
+    }
+
+}
